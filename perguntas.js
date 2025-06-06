bc36a86e31e4fe56650ec78eba89b698c0ee8925
cauã franco <162469449+CauaZERO9@users.noreply.github.com>
2025-06-06 08:38:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Copa do Mundo',
+    'Quem ganhou a copa do mundo de 2002?',
+    'Brasil'
+)
+
+criaCartao(
+    'Copa do Mundo',
+    'Quem foi o artilheiro da copa do mundo de 2022?',
+    'Kylian Mbappé'
+)
+
+criaCartao(
+    'Mundial de Clubes',
+    'Qual o time brasileiro não tem mundial?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Mundial de Clubes',
+    'Quem foi campeão do mundial de clubes em 2012?',
+    'Corinthians'
+)
